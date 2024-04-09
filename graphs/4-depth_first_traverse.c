092416c2907cc40d4802f5d381ffc8761b37fec0
Romain BAYLE <5014@holbertonstudents.com>
2024-04-09 18:06:07 +0000

    fixed again depth incrementation

--- graphs/4-depth_first_traverse.c
+++ graphs/4-depth_first_traverse.c
@@ -36,17 +36,17 @@ void visit_vertice(vertex_t *v, size_t depth,
 
 	if (depth > max_depth)
 		max_depth = depth;
-	depth++;
 	tmp = v->edges;
 	for (i = 0; i < v->nb_edges; i++)
 	{
 		if (is_visited(tmp->dest->index) == 0)
 		{
+			depth++;
 			visit_vertice(tmp->dest, depth, action);
+			depth--;
 		}
 		tmp = tmp->next;
 	}
-	depth--;
 }
 
 /**
