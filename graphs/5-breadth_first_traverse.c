0985685eb01c1009b6ec998224b731ec222683cf
Romain BAYLE <5014@holbertonstudents.com>
2024-04-15 15:21:57 +0000

    fixed allocation and frreing

--- graphs/5-breadth_first_traverse.c
+++ graphs/5-breadth_first_traverse.c
@@ -16,17 +16,20 @@ breadth_first_traverse(const graph_t *graph,
 {
 	edge_t *tmp_edge = NULL;
 	vertex_t *current_vertex = NULL;
-	vertex_t **queue = malloc(sizeof(vertex_t *) * graph->nb_vertices);
-	size_t *visited = calloc(graph->nb_vertices, sizeof(size_t));
-	size_t *depth = calloc(graph->nb_vertices, sizeof(size_t));
+	vertex_t **queue;
+	size_t *visited;
+	size_t *depth;
 	size_t front = 0, rear = 0, current_depth = 0, max_depth = 0;
 
-	if (!queue || !visited || !depth || !graph || !graph->vertices)
+	if (!graph || !graph->vertices)
 		return (0);
-
+	queue = malloc(sizeof(vertex_t *) * graph->nb_vertices);
+	visited = calloc(graph->nb_vertices, sizeof(size_t));
+	depth = calloc(graph->nb_vertices, sizeof(size_t));
+	if (!queue || !visited || !depth)
+		goto free_all;
 	queue[rear++] = graph->vertices;
 	visited[graph->vertices->index] = 1;
-
 	while (front != rear)
 	{
 		current_vertex = queue[front];
@@ -38,7 +41,7 @@ breadth_first_traverse(const graph_t *graph,
 		tmp_edge = current_vertex->edges;
 		while (tmp_edge)
 		{
-			if (visited[tmp_edge->dest->index] == 0)
+			if (!visited[tmp_edge->dest->index])
 			{
 				visited[tmp_edge->dest->index] = 1;
 				queue[rear] = tmp_edge->dest;
@@ -48,8 +51,12 @@ breadth_first_traverse(const graph_t *graph,
 			tmp_edge = tmp_edge->next;
 		}
 	}
-	free(visited);
-	free(queue);
-	free(depth);
+free_all:
+	if (visited)
+		free(visited);
+	if (queue)
+		free(queue);
+	if (depth)
+		free(depth);
 	return (max_depth);
 }
