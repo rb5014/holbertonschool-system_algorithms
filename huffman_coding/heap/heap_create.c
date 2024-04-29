309c86a6bd42e563011a93392f8898ded7358265
Romain Bayle <5014@holbertonstudents.com>
2024-04-29 16:51:54 +0200

    huffman_coding - heap_create

---
+++ huffman_coding/heap/heap_create.c
@@ -0,0 +1,28 @@
+#include "heap.h"
+
+/**
+ * heap_create - creates a Heap data structure
+ * This function takes two void * parameters,
+ * each one being the data stored in a node.
+ * This function will return the difference between
+ * the two data stored at those pointers.
+ * It will be used to determine if a node’s value is
+ * greater or lower than another, so we can build our heap
+ * root must be set to NULL
+ * size must be set to 0
+ * @data_cmp: pointer to a comparison function
+ * Return: pointer to the created heap_t structure, or NULL if it fails
+*/
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *new = malloc(sizeof(heap_t));
+
+	if (!new)
+		return (NULL);
+
+	new->root = NULL;
+	new->size = 0;
+	new->data_cmp = data_cmp;
+
+	return (new);
+}
