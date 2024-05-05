fb805b537b08becc51fd4d4bbfeea5d19a62c745
Romain Bayle <5014@holbertonstudents.com>
2024-05-05 16:50:51 +0200

    huffman_priority_queue

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include "heap.h"
 /**
  * struct symbol_s - Stores a char and its associated frequency
  *
@@ -16,5 +17,5 @@ typedef struct symbol_s
 } symbol_t;
 
 symbol_t *symbol_create(char data, size_t freq);
-
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 #endif /* HUFFMAN_H */
---
+++ huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,52 @@
+#include "huffman.h"
+
+/**
+ * data_cmp - Compares two symbols frequency
+ *
+ * @p1: First pointer
+ * @p2: Second pointer
+ *
+ * Return: Difference between the frequecies
+ */
+int data_cmp(void *p1, void *p2)
+{
+	symbol_t *s1, *s2;
+
+	s1 = (symbol_t *)p1;
+	s2 = (symbol_t *)p2;
+
+	return ((int)(s1->freq - s2->freq));
+}
+
+/**
+ * huffman_priority_queue - creates a priority queue for
+ * the Huffman coding algorithm
+ * @data: array of characters of size size
+ * @freq: array containing the associated frequencies (of size size too)
+ * @size: size of the arrays
+ * Return: pointer to the created min heap (also called priority queue)
+ * Each node of this priority queue stores
+ * a heap leaf node containing a symbol_t *
+ *		node->data is a binary_tree_node_t * (let’s call it nested)
+ *		nested->data is our symbol_t *
+*/
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+	heap_t *heap;
+	binary_tree_node_t *nested;
+	symbol_t *symbol;
+	size_t i;
+
+	if (size == 0)
+		return (NULL);
+
+	heap = heap_create(data_cmp);
+
+	for (i = 0; i < size; i++)
+	{
+		symbol = symbol_create(data[i], freq[i]);
+		nested = binary_tree_node(NULL, symbol);
+		heap_insert(heap, nested);
+	}
+	return (heap);
+}
