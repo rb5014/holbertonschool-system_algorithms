513960f3bc287cceaefb5d87d7c573c3d52bfc23
Romain Bayle <5014@holbertonstudents.com>
2024-05-07 14:33:47 +0200

    fixed seg fault by storing the nested root to return

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -4,21 +4,25 @@
  * @data: array of characters of size size
  * @freq: array containing the associated frequencies (of size size too)
  * @size: size of the two arrays
- * Return: pointer to the root node of the Huffman tree, or NULL if it fails
+ * Return: pointer to the nested root node of the Huffman tree,
+ * or NULL if it fails
 */
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
 	heap_t *min_heap = huffman_priority_queue(data, freq, size);
+	binary_tree_node_t *nested;
 
 	if (min_heap == NULL)
 		return (NULL);
+
 	while (min_heap->size > 1)
 	{
 		if (huffman_extract_and_insert(min_heap) == 0)
 			return (NULL);
 
 	}
+	nested = min_heap->root->data;
 	free(min_heap->root);
 	free(min_heap);
-	return (min_heap->root->data);
+	return (nested);
 }
