e8c430cedd79455e06a957abf30ddbbaa0472233
Romain Bayle <5014@holbertonstudents.com>
2024-06-25 16:17:16 +0200

    nary_tree added feature: delete tree

--- nary_trees/1-nary_tree_delete.c
+++ nary_trees/1-nary_tree_delete.c
@@ -1,1 +1,22 @@
 #include "nary_trees.h"
+
+/**
+ * nary_tree_delete - deallocates an entire N-ary tree
+ * @tree: pointer to tree to delete
+*/
+void nary_tree_delete(nary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	free(tree->content);
+
+	if (tree->children)
+		nary_tree_delete(tree->children);
+
+	if (tree->next)
+		nary_tree_delete(tree->next);
+
+	free(tree);
+	tree = NULL;
+}
